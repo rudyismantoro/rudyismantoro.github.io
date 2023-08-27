9a0dd36e764b37c627d51359998eeee7e824e509
nursalimrozak <nursalimrozak@gmail.com>
2023-08-27 20:17:27 +0700

    Main
    90% Complete

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 		      error: function() {
 
 		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
+		      	$('#message-warning').html("Ada yang salah nih... Coba lagi ya...");
 		         $('#message-warning').fadeIn();
 
 		      }
